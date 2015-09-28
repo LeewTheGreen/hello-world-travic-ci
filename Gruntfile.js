557fd68374d142d917205c7b32607e9a147346e8
Igor Lauxen <igor.lauxen@sap.com>
2015-09-28 20:23:07 -0300

    option was not a recoginized command

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
     	all: ['Gruntfile.js', 'src/main/javascript/*.js','helloworld.js']
+		options: {
+	      // more options here if you want to override JSHint defaults
+	  		globals: {
+	  			jQuery: true,
+	  			console: true,
+	  			module: true
+		    }
+		  }
     }
-  	options: {
-      // more options here if you want to override JSHint defaults
-  		globals: {
-  			jQuery: true,
-  			console: true,
-  			module: true
-	    }
-	  }
   });
  
   grunt.loadNpmTasks('grunt-contrib-jshint');
