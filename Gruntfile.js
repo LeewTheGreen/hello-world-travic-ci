52bfcec50b4eef2b0b287b7988b5162eedddcfcd
Igor Lauxen <igor.lauxen@sap.com>
2015-09-28 20:16:26 -0300

    js files should be found under the folder javascript: changed the folder configuration on the grunt.initConfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
  
   grunt.initConfig({
     jshint: {
-    	all: ['Gruntfile.js', 'src/**/*.js','helloworld.js']
+    	all: ['Gruntfile.js', 'src/**/**/*.js','helloworld.js']
     }
   	options: {
       // more options here if you want to override JSHint defaults
